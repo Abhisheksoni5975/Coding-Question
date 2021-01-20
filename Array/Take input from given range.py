40da7ddf1dca40fe2f206df5706650c0e683363b
Abhishek Soni <59801358+Abhisheksoni5975@users.noreply.github.com>
2021-01-20 23:30:48 +0530

    Create Take input from given range.py

---
+++ Array/Take input from given range.py
@@ -0,0 +1,11 @@
+# Create list of numbers with given range
+
+r1= int(input()) # by default input function take string to use as inter use typecasting int(input())
+r2=int(input())
+a=[]
+if(r1==r2):
+   print(r1)
+while(r1<=r2):
+   a.append(r1)
+   r1=r1+1
+print(a)    
